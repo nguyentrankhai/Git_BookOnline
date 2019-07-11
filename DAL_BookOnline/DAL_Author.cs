57db7baa62799da0bfd344083025d0c726de1f35
DESKTOP-B4KBD2K\trinh <trinh199724@gmail.com>
2019-07-11 17:31:22 +0700

    Revert "Revert "Thêm hàm Insert cho các đối tượng""
    This reverts commit 9307f330a75cfe211b3a03689d9c905bcdee5c6d.

---
+++ DAL_BookOnline/DAL_Author.cs
@@ -0,0 +1,70 @@
+﻿using DTO_BookOnline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_BookOnline
+{
+    public class DAL_Author
+    {
+        DataClassesBookOnlineDataContext db = new DataClassesBookOnlineDataContext();
+        public List<Author> getAllAuthor()
+        {
+            List<Author> lstAuthor = new List<Author>();
+            try
+            {
+                var auths = db.tbl_Authors.ToList();
+                if (auths == null) return null;
+
+                List<Author> lstauth = new List<Author>();
+                foreach (tbl_Author auth in auths)
+                {
+                    lstauth.Add(parseSQLtoDTO(auth));
+                }
+                return lstauth;
+            }
+            catch
+            {
+
+            }
+            return lstAuthor;
+        }
+        private Author parseSQLtoDTO(tbl_Author o)
+        {
+            Author auth = new Author();
+            if (o != null)
+            {
+                auth.Name = o.AuthorNAME;
+                auth.Id = o.AuthorID;
+            }
+            return auth;
+        }
+        public bool insertAuthor(Author auth)
+        {
+            bool result = false;
+            try
+            {
+                DataClassesBookOnlineDataContext context = new DataClassesBookOnlineDataContext();
+                tbl_Author us = new tbl_Author();
+                us.AuthorID = auth.Id;
+                us.AuthorNAME = auth.Name;
+                us.BIRTHDAY = auth.Date;
+                us.COUNTRY = auth.Country;
+                us.GEN = auth.Gen;
+                us.SUMMARY = auth.Summary;
+                us.IMG = auth.Img;
+                us.NOTE = auth.Note;
+                context.tbl_Authors.InsertOnSubmit(us);
+                context.SubmitChanges();
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                System.Console.Write(ex.Message);
+            }
+            return result;
+        }
+    }
+}
